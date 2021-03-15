aff6a0ab2df05421628c86de1d2254f6987f4cfe
I an <ydh8485@gmail.com>
2021-03-15 23:13:05 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -12,21 +12,9 @@ function App() {
 
   return (
     <>
-      <div>
-        {name === "React" ? (
-          <h1>{name}-review</h1>
-        ) : (
-          <h1>리액트가 아님</h1>
-        )}
-      </div>
-      <h2>working well</h2>
-      <div>{name === "React" && <h1>React</h1>}</div>
-      <div>{noting || "noting = undefined"}</div>
-      <div style={{ color: "red", fontSize: "2rem" }}>
-        color: red
-      </div>
-      <div style={colorBlue}>color: blue</div>
-      <MyComponent data={"props 받음"} />
+      <MyComponent name={"React"} favoriteNumber={23}>
+        리액트
+      </MyComponent>
     </>
   );
 }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = (props) => {
+// 함수 파라미터 부분에서 비구조화 할당(구조 분해)
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <>
-      <div>props 받기 : {props.data}</div>;
-      <div>props 받기 : {props.data2}</div>;
+      <div>
+        props값은 {name} <br />
+        children값은 {children}
+        입니다.
+        <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
     </>
   );
 };
 
 MyComponent.defaultProps = {
   data2: "props 기본값",
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
