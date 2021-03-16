a3542c2a8ef970472599ec436a6b40821cfdee2a
I an <ydh8485@gmail.com>
2021-03-16 23:47:12 +0900

    ref to input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Counter from "./Counter";
 import EventPractice from "./EventPractice";
 import MyComponent from "./MyComponent";
@@ -6,59 +6,61 @@ import Say from "./Say";
 import "./App.css";
 import ValidationSample from "./ValidationSample";
 
-function App() {
-  const name = "React";
-  // undefined값을 return해서는 안된다. 단 jsx안에서는 허용된다.
-  const noting = undefined;
-  // 리액트에서 DOM요소에 스타일을 적용할 때는 문자열이 아니 객체형태로 넣어주어야 함(카멜 표기법)
-  // 미리 스타일 객체 선언
-  const colorBlue = {
-    color: "blue",
-    fontSize: "3rem",
-    background: "yellow",
-  };
+class App extends Component {
+  render() {
+    const name = "React";
+    // undefined값을 return해서는 안된다. 단 jsx안에서는 허용된다.
+    const noting = undefined;
+    // 리액트에서 DOM요소에 스타일을 적용할 때는 문자열이 아니 객체형태로 넣어주어야 함(카멜 표기법)
+    // 미리 스타일 객체 선언
+    const colorBlue = {
+      color: "blue",
+      fontSize: "3rem",
+      background: "yellow",
+    };
 
-  return (
-    <>
-      <h1 className="title">React</h1>
-      <h1 className="sub-title">review</h1>
+    return (
+      <>
+        <h1 className="title">React</h1>
+        <h1 className="sub-title">review</h1>
 
-      <section className="section">
-        <h1 className="summary">props</h1>
-        {/* 부모 컴포넌트인 App컴포넌트에서 자식 컴포넌트인 MyComponent에게 props전달 */}
-        <MyComponent name={"React"} favoriteNumber={23}>
-          {/* 미리 선언된 스타일 객체 style값으로 지정 */}
-          <span style={colorBlue}>리액트{name}</span>
-          {/* 태그는 항상 닫혀야 한다. */}
-          <br />
-          {/* 인라인 스타일링 */}
-          <span style={{ color: "red" }}>리액트</span>
-        </MyComponent>
-        {/* 값이 undefined일 때 보여줄 문자열 */}
-        <div>{noting || "undefined"}</div>
-      </section>
+        <section className="section">
+          <h1 className="summary">props</h1>
+          {/* 부모 컴포넌트인 App컴포넌트에서 자식 컴포넌트인 MyComponent에게 props전달 */}
+          <MyComponent name={"React"} favoriteNumber={23}>
+            {/* 미리 선언된 스타일 객체 style값으로 지정 */}
+            <span style={colorBlue}>리액트{name}</span>
+            {/* 태그는 항상 닫혀야 한다. */}
+            <br />
+            {/* 인라인 스타일링 */}
+            <span style={{ color: "red" }}>리액트</span>
+          </MyComponent>
+          {/* 값이 undefined일 때 보여줄 문자열 */}
+          <div>{noting || "undefined"}</div>
+        </section>
 
-      <section className="section setion__state">
-        <h1 className="summary">state</h1>
-        <Counter />
-      </section>
+        <section className="section setion__state">
+          <h1 className="summary">state</h1>
+          <Counter />
+        </section>
 
-      <section className="section setion__event1">
-        <h1 className="summary">event1</h1>
-        <Say />
-      </section>
+        <section className="section setion__event1">
+          <h1 className="summary">event1</h1>
+          <Say />
+        </section>
 
-      <section className="section setion__event2">
-        <h1 className="summary">event2</h1>
-        <EventPractice />
-      </section>
+        <section className="section setion__event2">
+          <h1 className="summary">event2</h1>
+          <EventPractice />
+        </section>
 
-      <section className="section section__ref">
-        <h1 className="summary">ref</h1>
-        <ValidationSample />
-      </section>
-    </>
-  );
+        <section className="section section__ref">
+          <h1 className="summary">ref</h1>
+          <ValidationSample />
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -19,13 +19,22 @@ class ValidationSample extends Component {
       clicked: true,
       validated: this.state.password === "0000",
     });
+    this.input2.focus();
   };
 
   render() {
     return (
       <div>
         <div>0000이면 success</div>
-        <input type="password" value={this.state.password} onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? "success" : "failure") : ""} />
+        {/* 리액트에서 DOM 접근없이 state로 input 검증, vanilla-js와 jQuery에서는 DOM에 접근해야 한다. */}
+        {/* 검증버튼 클릭시 focus가 input에 남아있지 않는다. ref로 DOM에 접근해 focus를 다시 돌려주는 작업, 콜백함수 방식 */}
+        <input
+          ref={(ref) => (this.input2 = ref)}
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={this.state.clicked ? (this.state.validated ? "success" : "failure") : ""}
+        />
         <button onClick={this.handleButtonClick}>검증하기</button>
       </div>
     );
