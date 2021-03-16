abaa9f9e8640c6dc6281293f6aa305b594865797
I an <ydh8485@gmail.com>
2021-03-16 15:41:11 +0900

    class-component state-update

--- src/Counter.js
+++ src/Counter.js
@@ -16,6 +16,16 @@ class Counter extends Component {
         <button
           onClick={() => {
             this.setState({ number: number + 1 }); // this.setState를 통해 state에 새로운 값을 넣을 수 있다.
+            // 객체 대신 함수 인자 전달, 비동기처리로 인한 state 값 업데이트 지연 방지
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            this.setState({ number: number + 1 }, () => {
+              console.log("this.setState가 끝난 후 특정 작업 실행");
+              console.log(this.state);
+            });
           }}
         >
           +1
