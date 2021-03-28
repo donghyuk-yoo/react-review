e2dd37a63e00ac94565b804c0a78dcb45dc7df3a
I an <ydh8485@gmail.com>
2021-03-28 18:14:34 +0900

    CSS Module SCSS

--- src/components/CSSModule.js
+++ src/components/CSSModule.js
@@ -1,9 +1,11 @@
 import React from "react";
-import styles from "../CSSModule.module.css";
+import styles from "../CSSModule.module.scss";
 
 function CSSModule() {
   return (
     // 템플릿 리터럴(백틱)으로 문자열을 합해 여러 클래스 적용
+    // 템플릿 리터럴 문법 미사용시
+    // className={[styles.wrapper, styles.inverted].join(' ')}
     <div className={`${styles.wrapper} ${styles.inverted}`}>
       hi, i'm <span className="something">CSS Module</span>
     </div>
